9b80cc1c647ab27c72839c4df360417e856c884c
Gabriel Antunes <gbr.antunes@gmail.com>
2023-07-05 11:54:19 -0300

    Initial commit

---
+++ src/mocks/products.ts
@@ -0,0 +1,43 @@
+export const products = [
+  {
+    _id: '649cd6a4f48e713eb00489c1',
+    name: 'Pizza quatro queijos',
+    description: 'Deliciosa pizza quatro queijos',
+    imagePath: '1688000164799-quatro-queijos.png',
+    price: 40,
+    ingredients: [
+      {
+        name: 'Mussarela',
+        icon: '🧀',
+        _id: '649cd6a4f48e713eb00489c2',
+      },
+      {
+        name: 'Parmesão',
+        icon: '🧀',
+        _id: '649cd6a4f48e713eb00489c3',
+      },
+      {
+        name: 'Gouda',
+        icon: '🧀',
+        _id: '649cd6a4f48e713eb00489c4',
+      },
+      {
+        name: 'Brie',
+        icon: '🧀',
+        _id: '649cd6a4f48e713eb00489c5',
+      },
+    ],
+    category: '649cab7ab41fcb7cf48e144d',
+    __v: 0,
+  },
+  {
+    _id: '649cd86d67eaf98f6e550cec',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1688000621878-coca-cola.png',
+    price: 7,
+    ingredients: [],
+    category: '649cab94b41fcb7cf48e1450',
+    __v: 0,
+  },
+]
